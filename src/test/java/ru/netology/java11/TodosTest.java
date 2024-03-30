ba22284a2d23b0e69d0a10e6113125b7e22f9d0f
Станислав Койфман <stanislavkoyfman@gmail.com>
2024-03-30 14:18:51 +0300

    Add few task, zero task and one task tests for Todos search

--- src/test/java/ru/netology/java11/TodosTest.java
+++ src/test/java/ru/netology/java11/TodosTest.java
@@ -34,10 +34,41 @@ class TodosTest {
     }
 
     @Test
-    public void TestSearch() {
+    public void TestAllTaskSearch() {
 
         Task[] expected = {simpleTask, epic, meeting};
         Task[] actual = todos.search("о");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestZeroTaskSearch() {
+
+        Task[] expected = {};
+        Task[] actual = todos.search("45345");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestOneTaskSearch() {
+
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("3");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestFewTaskSearch() {
+        SimpleTask simpleTask2 = new SimpleTask(15, "Несколько задач");
+
+        String[] subtasks2 = {"Несколько задач", "Яйца", "Хлеб"};
+        Epic epic2 = new Epic(155, subtasks2);
+
+        todos.add(simpleTask2);
+        todos.add(epic2);
+
+        Task[] expected = {simpleTask2, epic2};
+        Task[] actual = todos.search("Несколько задач");
         Assertions.assertArrayEquals(expected, actual);
     }
 }
